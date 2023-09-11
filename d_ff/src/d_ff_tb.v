dbd9df8451497ab94c41fb45793f1c0d8d717db0
Asfar <khanasfar73@gmail.com>
2023-09-11 16:35:09 -0400

    Create d_ff_tb.v

---
+++ d_ff/src/d_ff_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+
+module d_ff_tb();
+    reg clk, din, n_rst;
+    wire q, d1, clk1;
+    d_ff UUT(q, clk, n_rst, din);
+    assign d1   = din;
+    assign clk1 = clk;
+    initial
+        clk = 1'b0;
+        always
+        #10 clk = ~clk;
+        initial
+        begin
+            din       = 1'b0;
+            n_rst     = 1'b1;
+            #20 n_rst = 1'b0;
+            #10 din   = 1'b1;
+            #20 n_rst = 1'b1;
+            #18 din   = 1'b0;
+            #1 din    = 1'b1;
+            #20 din   = 1'b0;
+            #10;
+        end
+endmodule
+    
